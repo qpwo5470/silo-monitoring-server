e033bb412479089680af1436b2738d9a3992defd
qpwo5470 <qpwo5470@gmail.com>
2021-07-26 01:16:01 +0900

    initial commit

---
+++ post.php
@@ -0,0 +1,32 @@
+<?php
+$device = $_POST['device_name'];
+$data = $_POST['data'];
+
+date_default_timezone_set("Asia/Seoul");
+
+$time = time();
+
+
+$conn = mysqli_connect(
+    'localhost',
+    'silo',
+    'silo',
+    'device_state');
+
+$sql = "INSERT INTO states(device_name, data) VALUES ('$device', '$data')";
+mysqli_query($conn, $sql);
+mysqli_close($conn);
+
+
+$datetime = date('Y-m-d H:i:s', $time);
+$prepend = "[{$datetime}]\t";
+$json = json_decode($data, true);
+foreach($json as $key => $value){
+    $append = strtoupper($key)." : ".$value."\t";
+    $prepend = $prepend.$append;
+}
+
+
+$file = $device.'_log.txt';
+$fileContents = file_get_contents($file);
+file_put_contents($file, $prepend . $fileContents);
