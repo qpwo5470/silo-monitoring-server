2524888d594d3821ab52218a39f585e0235c9b8a
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 03:31:55 +0900

    web

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function loadSQL() {
         url: 'monitor.php',
         data: dataObj,
         success: function (result) {
-            response = result.split(/(?!}{)/);
+            response = result.split(/}(?={)/);
         },
         error: function () {
         }
