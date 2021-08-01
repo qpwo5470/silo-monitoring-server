45fe64c324d5a9d89c041bbbd2418b6caca8a3f8
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 04:06:50 +0900

    web

--- post.php
+++ post.php
@@ -28,7 +28,7 @@ $sql = "SELECT * FROM states WHERE device_name = '$device'";
 
 $exists = mysqli_query($conn, $sql);
 if (mysqli_fetch_array($exists)) {
-    $sql = "UPDATE states SET data = 'shit' WHERE device_name = '$device'";
+    $sql = "UPDATE states SET data = '$data' WHERE device_name = '$device'";
 }
 else {
     $sql = "INSERT INTO states(device_name, data) VALUES ('$device', '$data')";
