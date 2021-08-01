743717a2189a942b205f7d2637f2a6cdcb3d43a4
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 03:12:24 +0900

    web

--- monitor.php
+++ monitor.php
@@ -9,7 +9,7 @@ $sql = "SELECT * FROM states";
 
 $data = mysqli_query($conn, $sql);
 
-while ($datum = mysqli_fetch_array($data)) {
+while ($datum = mysqli_fetch_assoc($data)) {
     echo json_encode($datum);
 }
 ?>
