0ab3cf228441e8c75f08a1562bffa02565116b28
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 04:25:56 +0900

    web

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ function appendDevice(jsonData){
     let ul_list = $("#device_list");
     let data = JSON.parse(jsonData['data'].replace(/\\/, ''));
 
-    let HTMLData = "<h1>" + jsonData['device_name'] + "</h1>";
-    HTMLData += "<h2>" + data['app'] + "</h2>";
-    HTMLData += "<h3>Last Data : " + jsonData['time'] + "</h3>";
 
     let li = $("#"+jsonData['device_name']);
     if (li.length){
-        li.textContent = HTMLData;
+        li.children('#device_name').textContent = jsonData['device_name']
+        li.children('#app').textContent = jsonData['app']
+        li.children('#last_data').textContent = jsonData['time']
     }
     else {
+        let HTMLData = "<h1 id='device_name'>" + jsonData['device_name'] + "</h1>";
+        HTMLData += "<h2 id='app'>" + data['app'] + "</h2>";
+        HTMLData += "<h3 id='last_data'>Last Data : " + jsonData['time'] + "</h3>";
         ul_list.append("<li id=\"" + jsonData['device_name'] + "\">" + HTMLData + "</li>");
-        print('b');
     }
-    print('.');
 }
 
 let response = '';
