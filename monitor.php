aefc3421c5319675ea6f1947042b1cb1e9708a5f
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 03:10:32 +0900

    web

--- monitor.php
+++ monitor.php
@@ -10,6 +10,6 @@ $sql = "SELECT * FROM states";
 $data = mysqli_query($conn, $sql);
 
 while ($datum = mysqli_fetch_assoc($data)) {
-    echo str_replace(json_encode($datum), '\\\"', '\"');
+    echo str_replace(json_encode($datum), '\\"', '"');
 }
 ?>
