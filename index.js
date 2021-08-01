ea872ff887b9a9b4f007bd569e7c7ce7f6c7795a
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 03:56:26 +0900

    web

--- index.js
+++ index.js
@@ -15,15 +15,20 @@ function loadSQL() {
 }
 
 function appendDevice(jsonData){
-    var ul_list = $("#device_list");
+    let ul_list = $("#device_list");
+    let data = JSON.parse(jsonData['data'].replace(/\\/, ''));
 
-    data = JSON.parse(jsonData['data'].replace(/\\/, ''));
+    let HTMLData = "<h1>" + jsonData['device_name'] + "</h1>";
+    HTMLData += "<h2>" + data['app'] + "</h2>";
+    HTMLData += "<h3>Last Data : " + jsonData['time'] + "</h3>";
 
-    HTMLData = "<h1>"+jsonData['device_name']+"</h1>";
-    HTMLData += "<h2>"+data['app']+"</h2>";
-    HTMLData += "<h3>Last Data : "+jsonData['time']+"</h3>";
-
-    ul_list.append("<li>"+HTMLData+"</li>");
+    let li = $("#"+jsonData['device_name']);
+    if (li.length){
+        li.value = HTMLData;
+    }
+    else {
+        ul_list.append("<li id=\"" + jsonData['device_name'] + "\">" + HTMLData + "</li>");
+    }
 }
 
 let response = '';
