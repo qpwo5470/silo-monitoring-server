53a6f57862e513935217f207a292418e8d4be341
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 06:08:33 +0900

    web

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function appendDevice(jsonData){
     let ul_list = $("#device_list");
     let data = JSON.parse(jsonData['data'].replace(/\\/, ''));
 
-    let HTMLData = "<div><h1 id='device_name'>" + jsonData['device_name'] + "</h1>";
-    HTMLData += "<h2 id='app'>" + data['app'] + "</h2>";
+    let HTMLData = "<div class='common'><h1 class='device_name'>" + jsonData['device_name'] + "</h1>";
+    HTMLData += "<h2 class='app'>" + data['app'] + "</h2>";
     let timePassed = Date.now()-Date.parse(jsonData['time'])
     let days,h,m,s;
     days = Math.floor(timePassed/1000/60/60/24);
@@ -30,11 +30,11 @@ function appendDevice(jsonData){
     if(m) stringTimePassed += m + "\" ";
     stringTimePassed += s + "s";
 
-    HTMLData += "<h3 id='last_data'>Last Data : " + stringTimePassed + "</h3></div><div>";
+    HTMLData += "<h3 class='last_data'>Last Data : " + stringTimePassed + "</h3></div><div class='uncommon'>";
 
     $.each(data,function(key, value){
         if(key !== 'app'){
-            HTMLData += "<h4 id='" + key + "'>" + key.toUpperCase() + " : " + value + "</h4>";
+            HTMLData += "<h4 class='" + key + "'>" + key.toUpperCase() + " : " + value + "</h4>";
         }
     });
     HTMLData += "</div>";
