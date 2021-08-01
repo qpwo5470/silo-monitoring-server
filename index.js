a1af46e2066e0460f317149f28b3ba20ba771ce9
qpwo5470 <qpwo5470@gmail.com>
2021-08-02 05:50:29 +0900

    web

--- index.js
+++ index.js
@@ -19,10 +19,18 @@ function appendDevice(jsonData){
     let HTMLData = "<div><h1 id='device_name'>" + jsonData['device_name'] + "</h1>";
     HTMLData += "<h2 id='app'>" + data['app'] + "</h2>";
     let timePassed = Date.now()-Date.parse(jsonData['time'])
-    console.log(Date.now())
-    console.log(Date.parse(jsonData['time']))
-    console.log('')
-    HTMLData += "<h3 id='last_data'>Last Data : " + timePassed.toString() + "</h3></div><div>";
+    let days,h,m,s;
+    days = Math.floor(timePassed/1000/60/60/24);
+    h = Math.floor((timePassed/1000/60/60/24 - days)*60);
+    m = Math.floor(((timePassed/1000/60/60/24 - days)*60 - h)*60);
+    s = Math.floor((((timePassed/1000/60/60/24 - days)*60 - h)*60 - m)*60);
+    let stringTimePassed = "";
+    if(days) stringTimePassed += days + "d ";
+    if(h) stringTimePassed += days + "h ";
+    if(m) stringTimePassed += days + "min ";
+    stringTimePassed += s + "sec";
+
+    HTMLData += "<h3 id='last_data'>Last Data : " + stringTimePassed + "</h3></div><div>";
 
     $.each(data,function(key, value){
         if(key !== 'app'){
